932bf174eb6a43afe0e68dda5d60b1e0e5f1af55
vucong2018 <vucong2018@gmail.com>
2024-06-03 12:27:19 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'
+    outDir: 'dist'
   },
-  // base: '/web-basic/',
+  base: '/web-basic/',
   resolve: {
     alias: {
       '@components': '/src/components',
